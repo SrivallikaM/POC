4d50bf58c3f06c69280357cae476b722cf8c7141
Srivallika <srivallika12345@gmail.com>
2019-08-27 11:03:12 -0400

    body

--- src/app/factsheet/factsheet/factsheet.component.ts
+++ src/app/factsheet/factsheet/factsheet.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-factsheet',
   templateUrl: './factsheet.component.html',
-  styleUrls: ['./factsheet.component.css']
+  styleUrls: ['./factsheet.component.css'],
+  //encapsulation:ViewEncapsulation.None
+  
 })
 export class FactsheetComponent implements OnInit {
 
